b7056e7aec4c845fb32bd644f385d52022f85526
Andrew Stevenson <ajstevenson@crimson.ua.edu>
2022-09-15 17:15:11 -0500

    search is working

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ function apiSearch() {
     "offset": "0",
     "mkt": "en-us"
   };
-
-  $.ajax({
-      url: 'my-api-url' + $.param(params),
+    $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "031ee0a3b16949de9c483968fea4391f");
       },
       type: "GET",
     })
@@ -21,11 +20,31 @@ function apiSearch() {
       for (i = 0; i < len; i++) {
         results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
       }
+        $('#searchResults').html(results);
 
-      $('#searchResults').html(results);
-      $('#searchResults').dialog();
+
+        $('#searchResults').dialog({
+            title: 'Results for "' + $("#query").val() + '"',
+            width: '90%',
+            close: closeFunction,
+            height: ($(window).height() - 75),
+            modal: true
+        });
+        results = '';
     })
     .fail(function () {
       alert("error");
     });
+}
+
+
+function searchClick() {
+    apiSearch();
+    document.getElementById("searchResults").style.visibility = "visible";
+}
+
+function closeFunction() {
+    document.getElementById("searchResults").style.visibility = "hidden";
+    document.getElementById("searchResults").innerHTML = "";
+    $("#query").val('');
 }
